d1e8bf4bf4e48c9caff28646de8828183afd3790
Martin Splitt <martin@geekonaut.de>
2019-01-17 10:35:22 +0100

    Adds util to get the local IP

---
+++ src/utils/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"errors"
+	"net"
+)
+
+func GetLocalIP() (net.IP, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP, nil
+			}
+		}
+	}
+	return nil, errors.New("No local IPv4 address for local network found")
+}
